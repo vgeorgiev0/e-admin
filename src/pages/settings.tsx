2d771cfe0cb393e3ca3fa70aad46db90dd5685a9
VGeorgiev <vgeorgiev@up2technology.com>
2023-06-13 11:01:55 +0300

    feat: Create a Settings screen

---
+++ src/pages/settings.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+type Props = {};
+
+const Settings = (props: Props) => {
+  return <Layout>Settings</Layout>;
+};
+
+export default Settings;
